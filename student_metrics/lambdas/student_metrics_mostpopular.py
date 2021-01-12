371eebaaf7d38032b050d5d3f6015729041cb70e
Richard Garcia <rgarcia@ubits.co>
2021-01-12 10:18:46 -0500

    Add the RDS instance grants to lambda

--- student_metrics/lambdas/student_metrics_mostpopular.py
+++ student_metrics/lambdas/student_metrics_mostpopular.py
@@ -1,10 +1,47 @@
 import json
+import boto3
+import pymysql
+
+s3 = boto3.client('s3')
 def handler(event, context):
-    print('request: {}'.format(json.dumps(event)))
-    return {
+    bucket = 'student-metrics'
+    key = 'courseMonth.json'
+
+    rds_host = 'moodle-dev-rds-aurora.cluster-c9maghmfm0zw.us-east-1.rds.amazonaws.com'
+    user_name = 'moodle_dev_admin'
+    passwd = 'S2zhSJAw4ZNm'
+    db_name = 'bitnami_moodle'
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+    content = response['Body']
+    jsonObject = json.loads(content.read())
+
+    course_id = jsonObject['course_id']
+    finished_count = jsonObject['finished_count']
+
+    print("El id del curso es " + str(course_id))
+    print("el total de terminados es " + str(finished_count))
+
+    # RDS connection
+    try:
+        conn = pymysql.connect(rds_host, user=user_name, passwd=passwd, db=db_name, connect_timeout=5)
+        print("Conexion exitosa!!")
+    except pymysql.MySQLError as e:
+        print("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        print("El error es " + e)
+
+    # Construct the body of the response object
+    responseBody = {
+        "course": course_id,
+        "finish": finished_count
+    }
+
+    response = {
         'statusCode': 200,
         'headers': {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'application/json'
         },
-        'body': 'Hello from mostPopular {}\n'.format(event['path'])
+        'body': json.dumps(responseBody)
     }
+
+    return response
--- student_metrics/student_metrics_stack.py
+++ student_metrics/student_metrics_stack.py
@@ -2,7 +2,10 @@ from aws_cdk import core
 from os import path
 from aws_cdk import (
     aws_lambda as _lambda,
-    aws_apigateway as _agw
+    aws_apigateway as _agw,
+    aws_s3 as _s3,
+    aws_ec2 as _ec2,
+    aws_rds as _rds
 )
 
 
@@ -14,11 +17,16 @@ class StudentMetricsStack(core.Stack):
         # The code that defines your stack goes here
         this_dir = path.dirname(__file__)
 
+        # Create the S3 bucket for JSON metrics files
+        student_bucket = _s3.Bucket(self, "student-metrics", bucket_name="student-metrics")
+
         # Lambda for mostPopular path
         lambda_mostpopular = _lambda.Function(
             self,
             'student_metrics_mostpopular',
-            code=_lambda.Code.from_asset(path.join(this_dir,'lambdas')),
+            function_name='student_metrics_mostpopular',
+            # code=_lambda.Code.from_asset(path.join(this_dir,'lambdas')),
+            code=_lambda.Code.from_asset(path.join(this_dir,'lambdas/mostpopular.zip')),
             handler='student_metrics_mostpopular.handler',
             runtime=_lambda.Runtime.PYTHON_3_8,
             description='Lambda to get information about the most popular course'
@@ -28,6 +36,7 @@ class StudentMetricsStack(core.Stack):
         lambda_coursemonth = _lambda.Function(
             self,
             'student_metrics_coursemonth',
+            function_name='student_metrics_coursemonth',
             code=_lambda.Code.from_asset(path.join(this_dir,'lambdas')),
             handler='student_metrics_coursemonth.handler',
             runtime=_lambda.Runtime.PYTHON_3_8,
@@ -38,11 +47,17 @@ class StudentMetricsStack(core.Stack):
         lambda_rankingcompany = _lambda.Function(
             self,
             'student_metrics_rankingcompany',
+            function_name='student_metrics_rankingcompany',
             code=_lambda.Code.from_asset(path.join(this_dir,'lambdas')),
             handler='student_metrics_rankingcompany.handler',
             runtime=_lambda.Runtime.PYTHON_3_8,
             description='Lambda to get information about the students ranking into the company'
         )
+
+        # Grant the bucket to read access from lambdas
+        student_bucket.grant_read(lambda_mostpopular)
+        student_bucket.grant_read(lambda_coursemonth)
+        student_bucket.grant_read(lambda_rankingcompany)
 
         api = _agw.RestApi(
             self,
@@ -69,18 +84,42 @@ class StudentMetricsStack(core.Stack):
         ranking_company_resource = metrics_resource.add_resource("rankingCompany")
 
         # Paths Methods
-        most_popular_method = most_popular_resource.add_method(
+        most_popular_resource.add_method(
             "GET",
             most_popular_integration
             # auth
         )
 
-        course_month_method = course_month_resource.add_method(
+        course_month_resource.add_method(
             "GET",
             course_month_integration
         )
 
-        ranking_company_method = ranking_company_resource.add_method(
+        ranking_company_resource.add_method(
             "GET",
             ranking_company_integration
         )
+
+        # RDS configuration
+
+        # RDS needs to be setup in a VPC
+        # vpc = _ec2.Vpc(self, 'vpc-students', max_azs=2)
+
+        # vpc_id = "vpc-4f3dd135"
+        # vpc = _ec2.Vpc.from_lookup(self, "VPC", vpc_id=vpc_id)
+
+        # security group
+        # sg = _ec2.SecurityGroup(self,'scg',vpc = vpc)
+
+        rds = _rds.DatabaseInstance.from_database_instance_attributes(
+            self,
+            id = 'db_instance',
+            instance_identifier = 'moodle-dev-rds-aurora',
+            instance_endpoint_address = 'moodle-dev-rds-aurora-instance-1.c9maghmfm0zw.us-east-1.rds.amazonaws.com',
+            port = 3036,
+            security_groups = [],
+            # .DatabaseInstanceEngine..mysql(version = _rds.AuroraMysqlEngineVersion.VER_5_7_12)
+            engine = _rds.AuroraMysqlEngineVersion.VER_5_7_12
+        )
+
+        rds.grant_connect(lambda_mostpopular)
