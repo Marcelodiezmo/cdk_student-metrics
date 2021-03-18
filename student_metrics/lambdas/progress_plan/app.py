a33a60575d635fc223a29ea75906d8d5f4ed423f
Jhoel Acosta L <jacosta@ubits.co>
2021-03-18 15:32:25 -0500

    ajusted lambda logic for Progress plan

--- student_metrics/lambdas/progress_plan/app.py
+++ student_metrics/lambdas/progress_plan/app.py
@@ -6,82 +6,47 @@ import os
 
 import constants
 from student_progress_plan import StudentProgressPlan
-from response import Response
+from response_factory import ResponseFactory, ResponseError
 
 s3 = boto3.client('s3')
 
-def exception_handler(response):
-    if response.error_message == None or response.error_message == '':
-        response.error_message = 'General Error'
-
-    responseBody = {
-        "code": response.code,
-        "error_message": response.error_message,
-    }
-
-    return {
-        'statusCode': response.code,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': json.dumps(responseBody)
-    }
-
-
-def get_param_id(event, paramId):
-    paramValue = None
-    try:
-        paramValue = str(event['pathParameters'][paramId])
-    except Exception:
-        paramValue = ''
-    finally:
-        return paramValue
-    
-
 def handler(event, context):
-    studen_param_id = get_param_id(event, constants.STUDENT_ID_PARAM)
+    student_param_id = get_param_id(event, constants.STUDENT_ID_PARAM)
     bucket = os.environ['bucket_name']
-    path = 'students/progress_training_plan/'
-    key = path + 'progress_plan.json'
-
-    response = Response()
-
+    path = constants.RESOURCE_PATH
+    key = path + constants.RESOURCE_FILE_NAME
+    
     try:
         responseS3 = s3.get_object(Bucket=bucket, Key=key)
         content = responseS3['Body']
         jsonObject = json.loads(content.read())
-        response.data = get_data_from_json_object(jsonObject, studen_param_id)
+        data = get_data_from_json_object(jsonObject, student_param_id)
+        response = ResponseFactory.ok_status(data)
 
-        response =  {
-            'statusCode': response.code,
-            'headers': {
-                'Content-Type': 'application/json'
-            },
-            'body': json.dumps(response.data, default=obj_dict)
-        }
-
-        return response
+        return response.toJSON()
 
     except botocore.exceptions.ClientError as error:
-        response.error_message = str(error.response['Error']['Message'])
-        response.code = str(error.response['ResponseMetadata']['HTTPStatusCode'])
+        error_message = str(error.response['Error']['Message'])
+        code = str(error.response['ResponseMetadata']['HTTPStatusCode'])
+        response = ResponseError(code, error_message)
         return exception_handler(response)
-    except Exception:
-        response.code = 404
+    except Exception as e:
+        response = ResponseError(404, e.args[0])
+        print('ERROR: ', e.args[0])
         return exception_handler(response)
+
+def get_param_id(event, paramId):
+    param_value = ''
+    try:
+        param_value = str(event['pathParameters'][paramId])
+    finally:
+        return param_value
 
 def get_data_from_json_object(iterableList, studentIdParam):
     if(studentIdParam != ''):
-        iterableList = filter(lambda record : find_student_by_id(record, studentIdParam), iterableList)
+        iterableList = filter(lambda record : str(record[constants.USER_ID]) == str(studentIdParam), iterableList)
     map_iterator = map(map_finished_courses, iterableList)
     return list(map_iterator)
-
-
-def obj_dict(obj):
-    return obj.__dict__
-    
-def find_student_by_id(record, studentIdParam):
-    return str(record[constants.USER_ID]) == str(studentIdParam)
 
 def map_finished_courses(record):
     student = StudentProgressPlan(
@@ -89,3 +54,40 @@ def map_finished_courses(record):
         record.get(constants.PROGRESS_PERCENT, 0)
     )
     return student
+
+def exception_handler(response):
+    if response.error_message == None or response.error_message == '':
+        response.error_message = 'General Error'
+
+    response = ResponseFactory.error_client(response.code, response).toJSON()
+    
+    print(response)
+    return response
+
+def test_get_data_from_json_object():
+    filepath = 'C:/Desarrollo/Proyectos/Ubits/student-metrics/student_metrics/test/lambdas/progress_plan/resource/progress_plan.json'
+    content = open(filepath + '', "r")
+    json_object = json.loads(content.read())
+    result = get_data_from_json_object(json_object, '')
+    # result = get_data_from_json_object(json_object, 3051)
+    print ('###################################')
+    print_iterator(result)
+    print(len(result))
+
+def test_handler():
+    param_id = ''
+    event = {'pathParameters':{constants.STUDENT_ID_PARAM : param_id}}
+    os.environ['bucket_name'] = 'student-metrics'
+    result = handler(event, None)
+    print (result)
+
+    
+def print_iterator(it):
+    for x in it:
+        print(x, end=' ')
+    print('')  # for new line
+
+if __name__ == '__main__':
+    test_get_data_from_json_object()
+    # test_handler()
+    
--- student_metrics/lambdas/progress_plan/constants.py
+++ student_metrics/lambdas/progress_plan/constants.py
@@ -2,3 +2,6 @@ STUDENT_ID_PARAM = 'studentId'
 
 USER_ID = 'Id_Usuario'
 PROGRESS_PERCENT = 'PorcentajeAvance'
+
+RESOURCE_PATH = 'students/progress_training_plan/'
+RESOURCE_FILE_NAME = 'progress_plan.json'
