539357e96b0508b20c5b23c31e59de847fac3dce
Mario Alzate <malzate@ubits.co>
2021-03-18 14:40:59 -0500

    Update the exception handler

--- student_metrics/lambdas/dashboard_powerbi/app.py
+++ student_metrics/lambdas/dashboard_powerbi/app.py
@@ -2,22 +2,27 @@ from powerBI_client import PowerBIClientService
 from response_factory import ResponseFactory, ResponseError
 
 
-def exception_handler(response):
-    if response.error_message is None or response.error_message == '':
-        response.error_message = 'General Error'
+# Handle exception and create response http object
+def exception_handler(e):
+    if len(e.args) > 1:
+        print('ERROR: ', e.args[0], e.args[1])
+        response = ResponseError(e.args[0], e.args[1])
+        response = ResponseFactory.error_client(response.code, response).toJSON()
+    else:
+        print('ERROR: ', e.args[0])
+        response = ResponseError(500, e.args[0] if e.args[0] != '' else 'General Error')
+        response = ResponseFactory.error_lambda(response).toJSON()
 
-    response = ResponseFactory.error_client(response.code, response).toJSON()
     return response
 
 
 def handler(event, context):
     print("Init Lambda")
     try:
-        response_body = PowerBIClientService.get_dashboard_url()
+        response_body = PowerBIClientService().get_dashboard_url()
         response = ResponseFactory.ok_status(response_body).toJSON()
         print(response)
+
         return response
     except Exception as e:
-        response = ResponseError(e.args[0], e.args[1])
-        print('ERROR: ', e.args[0], e.args[1])
-        return exception_handler(response)
+        return exception_handler(e)
