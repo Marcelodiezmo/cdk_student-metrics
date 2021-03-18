e41aaa754f5ebe12057775b1e88da5de8a7ac07f
Mario Alzate <malzate@ubits.co>
2021-03-18 11:21:12 -0500

    Change layer version

--- student_metrics/lambdas/dashboard_powerbi/app.py
+++ student_metrics/lambdas/dashboard_powerbi/app.py
@@ -1,20 +1,19 @@
-import powerBI_client as PowerBIClient
+from powerBI_client import PowerBIClientService
 from response_factory import ResponseFactory, ResponseError
 
 
 def exception_handler(response):
-    if response.error_message == None or response.error_message == '':
+    if response.error_message is None or response.error_message == '':
         response.error_message = 'General Error'
 
     response = ResponseFactory.error_client(response.code, response).toJSON()
-    print(response)
     return response
 
 
 def handler(event, context):
     print("Init Lambda")
     try:
-        response_body = PowerBIClient.PowerBIClientService().get_dashboard_url()
+        response_body = PowerBIClientService.get_dashboard_url()
         response = ResponseFactory.ok_status(response_body).toJSON()
         print(response)
         return response
