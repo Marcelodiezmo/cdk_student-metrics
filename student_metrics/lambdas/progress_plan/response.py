a0400dade56961a684f6ee727ac61bbb4f1cec1c
Jhoel Acosta L <jacosta@ubits.co>
2021-03-23 16:39:37 -0500

    removed all src of progress plan

--- student_metrics/lambdas/progress_plan/response.py
+++
@@ -1,6 +0,0 @@
-class Response:
-
-    def __init__(self):
-        self.code = 200
-        self.error_message = None
-        self.data = None
--- student_metrics/lambdas/progress_plan/student_progress_plan.py
+++
@@ -1,7 +0,0 @@
-class StudentProgressPlan:
-    def __init__(self, user_id, progress_percent):
-        self.user_id = user_id
-        self.progress_percent = progress_percent
-
-    def __str__(self):
-        return str(self.__class__) + ": " + str(self.__dict__)
