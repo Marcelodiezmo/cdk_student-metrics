8b1249ffbd67d0f1050fe0bb7dedb7022d341652
Jhoel Acosta L <jacosta@ubits.co>
2021-04-14 16:34:35 -0500

    changed resource path of progress plan to 'AvancePlanFormacion1'

--- student_metrics/lambdas/progress_plan/constants.py
+++ student_metrics/lambdas/progress_plan/constants.py
@@ -3,6 +3,6 @@ STUDENT_ID_PARAM = 'studentId'
 USER_ID = 'Id_Usuario'
 PROGRESS_PERCENT = 'PorcentajeAvance'
 
-RESOURCE_PATH = 'students/progress_training_plan/'
+RESOURCE_PATH = 'AvancePlanFormacion1/'
 RESOURCE_COMPANY_PATH = 'Id_Empresa={company_id}/'
 RESOURCE_FILE_NAME = 'progress_plan.json'
