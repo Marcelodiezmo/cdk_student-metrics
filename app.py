fc7dfa24d63fe57e1a965a081bf31aad279dedfb
Felipe Fonseca-Lamprea <afonseca@ubits.co>
2022-07-19 14:52:39 -0500

    Test stack added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from os import device_encoding
 from aws_cdk import core
 
 from student_metrics.student_metrics_stack import StudentMetricsStack
+from student_metrics.student_metrics_stack import StudentMetricsStackTEST
 
 prod = core.Environment(account='986361039434', region='us-east-1')
 dev = core.Environment(account='824404647578', region='us-east-1')
@@ -22,5 +23,17 @@ StudentMetricsStack(app, "student-metrics-dev", env=core.Environment(
     region='us-east-1'), stage='dev')
 
 # StudentMetricsStack(app, "student-metrics-main", stage='main')
+
+StudentMetricsStackTEST(app, "student-metrics-test-GOTOPROD", env=core.Environment(
+    account='986361039434',
+    region='us-east-1'), stage='test')
+
+StudentMetricsStackTEST(app, "student-metrics-GOTOPROD", env=core.Environment(
+    account='986361039434',
+    region='us-east-1'), stage='prod')
+
+StudentMetricsStackTEST(app, "student-metrics-dev-GOTOPROD", env=core.Environment(
+    account='824404647578',
+    region='us-east-1'), stage='dev')
 
 app.synth()
